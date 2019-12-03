df5e0d07f7e89e46cb987491868b4b838841e4ca
StanMan2000 <52841174+StanMan2000@users.noreply.github.com>
2019-12-02 18:10:12 -0600

    Add files via upload
    Import specific department data

--- import.py
+++ import.py
@@ -35,15 +35,23 @@ with open("undergrad.csv", "r") as file:
 
 # Read in CSV file
 with open(sys.argv[1], "r") as file:
+    """
     # Clear existing stored concentrations
-    #db.execute("DELETE FROM concentrations;")
-    # Store all of the general concentration info (first few columns)
+    db.execute("DELETE FROM concentrations;")
+    """
+    # Clear existing stored course requirements/exceptions
+    db.execute("DELETE FROM requirements;")
+    db.execute("DELETE FROM exceptions;")
+    # Store the list of concentration info
     concentrations = list(csv.DictReader(file))
     # Store the list of course requirements (file stream reset required)
-    #file.seek(0)
-    #req_courses = list(csv.reader(file))
+    file.seek(0)
+    all_req_courses = list(csv.reader(file))
     # Loop through concentrations
-    for concentration in range(len(concentrations)):
+    for concentration in range(4):#len(concentrations)):
+        # Stop at the blank line
+        if concentrations[concentration] == "":
+            sys.exit()
         """
         # Retrieve name and number of required courses
         name = concentrations[concentration]["Concentration"]
@@ -52,20 +60,37 @@ with open(sys.argv[1], "r") as file:
         db.execute("INSERT INTO concentrations (id,name, required) VALUES (?,?,?);", concentration + 1, name, required)
         """
         # Retrieve list of course requirements (from course header to first blank)
-        start = courses[0].index("Courses")
-        end = courses[concentration + 1].index("")
-        course_list = courses[concentration + 1][start:end]
+        print(concentrations[concentration]["Concentration"])
+        start = all_req_courses[0].index("Courses")
+        # Avoids error when no empty cell exists
+        try:
+            end = all_req_courses[concentration + 1].index("")
+            cell_list = all_req_courses[concentration + 1][start:end]
+        except ValueError:
+            cell_list = all_req_courses[concentration + 1][start:]
+
         # Import into SQL database
-        category = 0
-        for course in range(len(course_list)):
-            # Split by comma if multiple courses are accepted
-            course = course_list[course].split(",")
-            # Insert all courses into requirements or exceptions database
-            for i in range(len(course)):
-                if course[i][0] = "!"
-                    ##Insert into exceptions
-                else:
-                    ##db.execute("INSERT INTO requirements (course_id, conc_id, category) VALUES (?,?,?);", __________, concentration + 1, category)
-            # Update category number
-            category += 1
-            print(course)
+        # Each cell in the list contains 1+ courses
+        for cell in range(len(cell_list)):
+            if (cell_list[cell] == ""):
+                continue
+            else:
+                # Split by comma in case courses are in cell
+                courses = cell_list[cell].split(", ")
+                for course in range(len(courses)):
+                    # Check for a general exception (first cell element is !)
+                    if (courses[course][0] == "!"):
+                        exception_name = courses[course][1:]
+                        db.execute("INSERT INTO exceptions (conc_id, name, category) VALUES (?,?,?);", concentration + 1, exception_name, cell)
+                    # Department exception
+                    elif (courses[course][0] == "*"):
+                        dept_name = courses[course][1:]
+                        dept_matches = db.execute("SELECT code FROM courses WHERE code LIKE ?;", dept_name + "%")
+                        # Load every matching course into requirements
+                        for i in range(len(dept_matches)):
+                            req_name = dept_matches[i]["code"].upper()
+                            db.execute("INSERT INTO requirements (course_code, conc_id, category) VALUES (?,?,?);", req_name, concentration + 1, cell)
+                    # Enter the course as normal
+                    else:
+                        req_name = courses[course].upper()
+                        db.execute("INSERT INTO requirements (course_code, conc_id, category) VALUES (?,?,?);", req_name, concentration + 1, cell)
